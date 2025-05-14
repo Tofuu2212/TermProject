95a6f47d886abf6fbc4f6370e7aace067dd86c60
Tofuu2212 <111795781+Tofuu2212@users.noreply.github.com>
2025-05-13 23:44:54 -0700

    Update DrawObject class

--- src/Client/DrawObject/DrawObject.java
+++ src/Client/DrawObject/DrawObject.java
@@ -1,9 +1,24 @@
 package Client.DrawObject;
 
-public abstract class DrawObject {
+import java.awt.*;
+
+abstract class DrawObject {
     int x;
     int y;
     int id;
 
-    public abstract void draw();
+    public DrawObject(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getID() {
+        return id;
+    }
+
+    public void setID(int id) {
+        this.id = id;
+    }
+
+    public abstract void draw(Graphics2D g2d);
 }
