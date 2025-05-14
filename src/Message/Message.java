936fb6f543dba4c9353cfda28d7448daca4ac3e0
nathanzimet <nathan.zimet@gmail.com>
2025-05-13 19:40:56 -0700

    Updated Message to work with draw

--- src/Message/Message.java
+++ src/Message/Message.java
@@ -1,29 +1,27 @@
 package Message;
 
+//TODO: fill out constructor types and fix toString for easy debugging
+// update ClientMessageHandler to parse or add a new parser class like Server has
+
 import java.io.Serializable;
 
 public class Message implements Serializable {
-    //int clientID from;
-    String dummydata;
-    Type type;
+    int id;
+    boolean kill;
+    int x;
+    int y;
+
+    Type mainType;
+    Type subType;
 
     public Message() {
-        dummydata = "empty";
-        type = null;
+
     }
 
-    public Message(String dummydata, Type type) {
-        this.dummydata = dummydata;
-        this.type = type;
-    }
-
-    public String getDummydata() {
-        return dummydata;
-    }
-    public Type getType() { return type; }
+    public Type getType() { return null; }
 
     @Override
     public String toString() {
-        return ("type: " + type + ", data: " + dummydata);
+        return ("ok");
     }
 }
